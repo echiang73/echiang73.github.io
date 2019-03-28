731ab244d4635f7047f9301b9bf74aad6d03db1b
Neo <echiang73@gmail.com>
2019-03-28 13:40:12 -0700

    Upload working version with parallax

---
+++ assets/javascript/init.js
@@ -0,0 +1,38 @@
+// sticky header
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+
+// on click on a href link, will slowly scroll to tag
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    var target = $(this.hash);
+    if (target.length) {
+      $("html, body").animate(
+        {
+          scrollTop: target.offset().top
+        },
+        1000
+      );
+    }
+  });
+});
+
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
